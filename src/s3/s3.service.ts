5c53575ec1780636e354a68917b5675561749da9
Max Koval <kova1max99@gmail.com>
2022-05-24 14:56:31 +0300

    fix: s3.service minor fixes

--- src/s3/s3.service.ts
+++ src/s3/s3.service.ts
@@ -1,31 +1,39 @@
 import { Injectable } from '@nestjs/common';
 import { Response } from 'express';
 import { S3 } from 'aws-sdk';
+import { ServerResponse } from 'http';
 
 @Injectable()
 export class S3Service {
-  private readonly BUCKET = '';
+  private readonly BUCKET = 'elasticbeanstalk-us-east-2-695649667796';
 
   private s3 = new S3({
-    accessKeyId: '',
-    secretAccessKey: '',
+    accessKeyId: 'AKIA2D57TRLKLMOPDDFI',
+    secretAccessKey: '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',
   });
 
   public async upload(file: Express.Multer.File): Promise<any> {
-  	return this.s3.upload({
-  		Bucket: this.BUCKET,
+    return this.s3
+      .upload({
+        Bucket: this.BUCKET,
         Key: file.originalname,
-        Body: file,
-        ACL: "public-read",
+        Body: file.buffer,
+        ACL: 'public-read',
         ContentType: file.mimetype,
-  	})
+      })
+      .promise();
   }
-  
-  public async download(filename: string, res: Response): Promise<any> {
-    const stream = this.s3.getObject({
-    	Bucket: this.BUCKET,
-        Key: filename
-    }).createReadStream();
+
+  public async download(
+    filename: string,
+    res: Response,
+  ): Promise<ServerResponse> {
+    const stream = this.s3
+      .getObject({
+        Bucket: this.BUCKET,
+        Key: filename,
+      })
+      .createReadStream();
 
     res.attachment(filename);
 
